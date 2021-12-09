b8e7b48278a2ea4dd55147be5585fff3b7288575
Catia Jacob <catia.jacob@inovflow.local>
2021-12-09 15:22:12 +0000

    setUp

---
+++ SMStechMVCWebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SMStechMVCWebApplication.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(250)]
+        public string FirstName { get; set; }
+        [StringLength(250)]
+        public string LastName { get; set; }
+        [StringLength(250)]
+        public string Adress1 { get; set; }
+        [StringLength(250)]
+        public string Adress2 { get; set; }
+        [StringLength(50)]
+        public string PostCode { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ SMStechMVCWebApplication/Data/Migrations/20211209130956_ExtendASpNetUsersTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SMStechMVCWebApplication.Data.Migrations
+{
+    public partial class ExtendASpNetUsersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Adress1",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Adress2",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Adress1",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Adress2",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PostCode",
+                table: "AspNetUsers");
+        }
+    }
+}
