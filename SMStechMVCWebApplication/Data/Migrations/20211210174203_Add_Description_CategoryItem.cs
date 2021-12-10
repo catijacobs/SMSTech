f490decdb264e23367f8c09d6f9fccab4e946827
Catia Jacob <catia.jacob@inovflow.local>
2021-12-10 18:50:08 +0000

    CreateQuerieCategoryItemController

---
+++ SMStechMVCWebApplication/Data/Migrations/20211210174203_Add_Description_CategoryItem.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SMStechMVCWebApplication.Data.Migrations
+{
+    public partial class Add_Description_CategoryItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "CategoryItem",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "CategoryItem");
+        }
+    }
+}
